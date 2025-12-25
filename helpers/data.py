c405717f17cea99797a4b63ecfa023510e941ce6
= <=>
2025-12-25 15:53:13 -0800

    Updated Doc String. Removed os module in favor of Path. File parameter is path and forced to be path. Moved print to sys.exit() Raised error FileNotFoundError. Test expanded.

--- helpers/data.py
+++ helpers/data.py
@@ -1,36 +1,47 @@
 """
-Docstring for helpers.data
+data.py
 
-Functions:
------------
-- data(file: str): returns a DataFram from a .csv file or none if it isn't valid.
+This module takes a .csv file and returns a DataFrame.
+
+Dependencies:
+    - Requires pandas
 """
+from pathlib import Path
 import sys
-import os
 try:
     import pandas
     from pandas import DataFrame
 except ImportError:
-    print("Failed to import the packages.")
-    sys.exit()
+    sys.exit(f"Failed to import the packages. {__file__}")
 
-def data(file: str) -> DataFrame | None:
+def data(file: Path) -> DataFrame | None:
     """
-    Docstring for data
-    
-    :param file: Description
-    :type file: str
-    :return: Description
-    :rtype: DataFrame | None
+    Parameters:
+        file (Path): file should be .csv
+    Returns:
+        DataFrame: a dataframe to use for other functions.
+    Raises:
+        ImportError: if pandas isn't isntalled.
+        TypeError: if file isn't a Path
+        FileNotFoundError: if the file isn't found.
     """
-    if os.path.isfile(file):
+    if not isinstance(file, Path): # type: ignore
+        raise TypeError(f"file should be Path, got {type(file)}\n{__file__}")
+
+    if file.exists() and file.is_file():
         with open(file, "r", encoding = "utf-8") as f:
-            df = pandas.read_csv(f)
+            df = pandas.read_csv(f) # type: ignore
             return df
     else:
-        return None
+        raise FileNotFoundError(f"file doesn't exist or isn't file. {file}\n{__file__}")
 
 
 if __name__ == "__main__":
-    test = data("data_examples\\comments-status.csv")
-    print(test)
+    test_path = Path.cwd() / "data_examples" / "NotStarted.csv"
+    test = data(test_path)
+    if test is None:
+        sys.exit(f"Test variable is None, not DataFrame.")
+    else:
+        assert isinstance(test, DataFrame)
+        assert not test.empty
+        print("All tests passed!")
