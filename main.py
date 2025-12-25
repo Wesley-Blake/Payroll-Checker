fc7d7bdc776e296659c8084bad5daca64e4e1f5e
= <=>
2025-12-24 19:12:15 -0800

    Removed dailey hours from main.py.

--- main.py
+++ main.py
@@ -30,22 +30,3 @@ if __name__ == "__main__":
                              pay_period=PAY_PERIOD,
                              body=["Timesheet not started."])
 
-    # Daily Overtime Emails
-    hours_csv = max([csv for csv in os.listdir() if "ts_break_down" in csv])
-    email_csv = max([csv for csv in os.listdir() if "Active_Empls" in csv])
-    df_hours = data.data(DOWNLOADS / hours_csv)
-    df_email = data.data(DOWNLOADS / email_csv)
-
-    if df_hours.empty or df_email.empty or df_hours is None or df_email is None:
-        print("One of the dataframes is empty, no emails sent for excessive hours.")
-        sys.exit()
-    overtime_dict = excessive_hours.excessive_hours(df_hours, df_email)
-    for manager, employee in overtime_dict.items():
-        win32com_email.email(cc=manager,
-                             bcc=employee,
-                             pay_period=PAY_PERIOD,
-                             body=["""
-                                You have more hours allocated then should be recorded.
-                                Anything over 7.5 hours for union is either Overtime or an error.
-                                Anything over 8 hours for non-union is Overtime or an error.
-                                """])
