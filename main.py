e94bfbf277ed9cfd34df1e3e4b63e0a571a243d3
= <=>
2025-12-31 18:36:33 -0800

    Added overtime error script to main.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ try:
     import helpers.pay_period_detector as pay_period_detector
     import helpers.win32com_email as win32com_email
     import not_started
+    import over_eight_hours
 except ImportError:
     sys.exit("Failed to import helper funcitons.")
 
@@ -33,12 +34,23 @@ if __name__ == "__main__":
     os.chdir(DOWNLOADS)
     not_started_csv = max([csv for csv in os.listdir() if "Empls_who_not_yet_" in csv])
     not_started_df = data.data(DOWNLOADS / not_started_csv)
-    if not_started_df is not None:
-        emails_dict = not_started.not_started_list(not_started_df)
-        if emails_dict is not None:
-            for manager, employee in emails_dict.items():
-                win32com_email.email(cc=manager,
-                                     bcc=employee,
-                                     pay_period=PAY_PERIOD,
-                                     body=["Timesheet not started."])
+    emails_dict = not_started.not_started_list(not_started_df)
+    if emails_dict is not None:
+        for manager, employee in emails_dict.items():
+            win32com_email.email(cc=manager,
+                                 bcc=employee,
+                                 pay_period=PAY_PERIOD,
+                                 body=["Timesheet not started."])
 
+    # REG over 8 or 7.5 for union
+    overtime_csv = max([csv for csv in os.listdir() if "breakdown" in csv])
+    overtime_df = data.data(DOWNLOADS / overtime_csv)
+    email_csv = max([csv for csv in os.listdir() if "active" in csv])
+    email_df = data.data(DOWNLOADS / email_csv)
+    if overtime_df is not None:
+        emails_dict = over_eight_hours.over_eight_hours(overtime_df, email_df)
+        for manager, employee in emails_dict.items():
+            win32com_email.email(cc=manager,
+                                 bcc=employee,
+                                 pay_period=PAY_PERIOD,
+                                 body=["Overtime not allocated."])
