12379676fa4551973fe004ba505870f935902d21
wesblake@proton.me <wesblake@proton.me>
2026-01-02 15:18:01 -0800

    Style changes for readability.

--- not_started.py
+++ not_started.py
@@ -11,7 +11,10 @@ import sys
 try:
     from pandas import DataFrame
 except ImportError:
-    sys.exit(f"Failed to import the packages. {__file__}")
+    sys.exit(
+        f"Failed to import the packages. \
+            {__file__}"
+    )
 
 def not_started_list(df: DataFrame) -> dict[str, list[str]] | None:
     """
@@ -25,17 +28,20 @@ def not_started_list(df: DataFrame) -> dict[str, list[str]] | None:
     """
     # Check: is dataframe?
     if not isinstance(df, DataFrame): # type: ignore
-        raise TypeError(f"df should be a DataFrame, got {type(df)}.\n{__file__}")
+        raise TypeError(
+            f"df should be a DataFrame, got {type(df)}.\n{__file__}"
+        )
 
     # Function variables.
     result: dict[str, list[str]] = {}
     headers = df.columns.tolist()
 
     # Removed ECLS I don't care about.
-    target_df = df[(df[headers[16]] != "SS") &
-                   (df[headers[16]] != "SN") &
-                   (df[headers[16]] != "WW")
-                   ]
+    target_df = df[
+        (df[headers[16]] != "SS") &
+        (df[headers[16]] != "SN") &
+        (df[headers[16]] != "WW")
+    ]
     # Check: is empty?
     if target_df.empty:
         return None
@@ -45,20 +51,26 @@ def not_started_list(df: DataFrame) -> dict[str, list[str]] | None:
 
     # Check: is list and emails are string?
     if not isinstance(manager_emails, list):
-        raise ValueError(f"manager_emails is not list, got {type(manager_emails)}.\n{__file__}") #type: ignore
+        raise ValueError(
+            f"manager_emails is not list, got \
+                {type(manager_emails)}.\n{__file__}"
+        ) #type: ignore
     if not isinstance(manager_emails[0], str) or '@' not in manager_emails[0]:
-        raise ValueError(f"manager_email in manager_emails is not email. {manager_emails[0]}\n{__file__}")
+        raise ValueError(
+            f"manager_email in manager_emails is not email. \
+                {manager_emails[0]}\n{__file__}"
+        )
 
     # Populate the result dictionary.
     for manager_email in manager_emails: #type: ignore
         result.update({manager_email: []})
-        result[manager_email] += target_df[target_df[headers[18]] == manager_email]\
-                                [headers[20]].unique().tolist() #type: ignore
-    
+        employee_email_df = target_df[target_df[headers[18]] == manager_email][headers[20]]
+        employee_email_list = employee_email_df.unique().tolist() #type: ignore
+        result[manager_email] += employee_email_list
+
     if len(result) == 0:
         return None
-    else:
-        return result
+    return result
 
 if __name__ == "__main__":
     from pathlib import Path
@@ -72,13 +84,13 @@ if __name__ == "__main__":
         if test is None:
             sys.exit("Test is None.")
         else:
-            assert isinstance(test,dict)
+            assert isinstance(test, dict)
 
-            keys = [key for key in test.keys()]
+            keys = list(test.keys())
             assert isinstance(keys[0], str)
             assert '@' in keys[0]
 
-            values = [value for value in test.values()]
+            values = list(test.values())
             assert isinstance(values[0], list)
             assert '@' in values[0][0]
 
